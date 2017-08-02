c0eac6d0ab25d8007fd9ac4ecf2c06bc7f2f27bb
Jeffrey Forman <code@jeffreyforman.net>
2017-08-02 01:38:55 +0000

    fix missing double quotes in struct json definitions. add ambient temp, hvac state, and humidity to temperature struct

--- structs.go
+++ structs.go
@@ -2,23 +2,25 @@ package nestmon
 
 // Root of the NestAPI Response JSON
 type NestAPIResponse struct {
-	Devices    *Devices              `json:"devices,omitempty"`
-	Structures map[string]*Structure `json:"structures,omitempty"`
+	Devices    *Devices              `json:"devices",omitempty`
+	Structures map[string]*Structure `json:"structures,omitempty`
 }
 
 type Devices struct {
-	Thermostats map[string]*Thermostat `json:"thermostats,omitempty"`
+	Thermostats map[string]*Thermostat `json:"thermostats",omitempty`
 }
 
 type Structure struct {
-	Name     string `json:name,omitempty"`
-	TimeZone string `json:time_zone,omitempty"`
+	Name     string `json:"name",omitempty`
+	TimeZone string `json:"time_zone",omitempty`
 }
 
 type Thermostat struct {
-	Humidity        int    `json:humidity"`
-	Name            string `json:"name"`
-	SoftwareVersion string `json:"software_version"`
+	AmbientTemperatureF int    `json:"ambient_temperature_f"`
+	Humidity            int    `json:"humidity"`
+	HvacState           string `json:"hvac_state"`
+	Name                string `json:"name"`
+	SoftwareVersion     string `json:"software_version"`
 }
 
 type NestmonConfig struct {
